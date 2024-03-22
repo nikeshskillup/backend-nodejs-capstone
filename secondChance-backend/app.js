d561debf5ef4784ab5418ca931e0ed63ea012351
nikeshskillup <142885550+nikeshskillup@users.noreply.github.com>
2024-03-22 12:12:08 +0530

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 8 */
+/* jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,19 +6,20 @@ const pinoLogger = require('./logger');
 const path = require('path');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+connectToDatabase()
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+    .catch((e) => {
+        console.error('Failed to connect to DB', e);
+    });
 
 app.use(express.json());
 
@@ -43,9 +44,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
